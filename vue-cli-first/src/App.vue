5ed4362aab6a1e978524a12eac32641752cd7dca
AlessandroSaba1991 <sabaalessandro515@gmail.com>
2022-05-02 16:11:29 +0200

    add component title_

--- vue-cli-first/src/App.vue
+++ vue-cli-first/src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Title_/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Title_ from './components/Title.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Title_
   }
 }
 </script>
---
+++ vue-cli-first/src/components/Title.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="title">
+        <h1>La mia prima APP con Vue CLI</h1>
+    </div>
+</template>
+
+<script>
+export default {
+   name: 'Title_'
+}
+</script>
+
+<style scoped>
+.title{
+    color: red;
+}
+</style>
